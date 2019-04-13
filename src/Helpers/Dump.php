d70fcb96467fdd42ea535d388aa1eb22ffc464ba
vserifsaglam <vahit.serif119@gmail.com>
2019-04-13 14:40:31 +0300

    add dump test

--- src/Helpers/Dump.php
+++ src/Helpers/Dump.php
@@ -2,14 +2,25 @@
 
 
 namespace SwooleTW\Http\Helpers;
+
 use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 
 class Dump
 {
 
-    public static function dd(...$args) : bool
+    /**
+     *  dump variables if only HtmlDumper and VarCloner exists
+     *
+     * @param mixed ...$args
+     * @return bool
+     */
+    public static function dd(...$args): bool
     {
+
+        if (!class_exists(HtmlDumper::class) || !class_exists(VarCloner::class)) {
+            return false;
+        }
 
         // init the dumper and cloner
         $dumper = new HtmlDumper();
--- tests/Helpers/DumpTest.php
+++ tests/Helpers/DumpTest.php
@@ -3,8 +3,15 @@
 namespace SwooleTW\Http\Tests\Helpers;
 
 use PHPUnit\Framework\TestCase;
+use SwooleTW\Http\Helpers\Dump;
 
 class DumpTest extends TestCase
 {
 
+    public function testDumpSuccess(): void
+    {
+        $response = Dump::dd('aa', 'bb');
+
+        $this->assertTrue($response);
+    }
 }
